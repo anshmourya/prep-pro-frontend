35d34622fc8d3b1465a1b65cc1815761095879dd
ansh mourya <ansh@levitation.co.in>
2025-01-19 10:13:08 +0530

    Integrate Kinde authentication into the app; add login and register buttons in App component. Update dependencies in package.json and package-lock.json, and include .env in .gitignore.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+
+
 const App = () => {
+  const { login, register } = useKindeAuth();
   return (
-    <button className="bg-blue-500 text-white p-2 rounded-md">App</button>
+    <>
+      <div className="flex  gap-2 items-center justify-center h-screen">
+        <button onClick={() => login()} className="bg-blue-500 text-white p-2 rounded-md">Login</button>
+        <button onClick={() => register()} className="bg-blue-500 text-white p-2 rounded-md">Register</button>
+      </div>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KindeProvider clientId={import.meta.env.VITE_CLIENT_ID}
+      domain={import.meta.env.VITE_DOMAIN}
+      redirectUri={import.meta.env.VITE_REDIRECT_URI}
+      logoutUri={import.meta.env.VITE_LOGOUT_URI}>
+      <App />
+    </KindeProvider>
   </StrictMode>,
 )
